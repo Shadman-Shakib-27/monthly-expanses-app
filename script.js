ec193d37987b28af369fdce96da5fc4d955aa521
Shadman-Shakib-27 <S.S.A.27.8.1999@gmail.com>
2022-02-17 04:00:14 +0600

    Half Done.

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('calculate-btn').addEventListener('click',function(){
+   const foodInput = document.getElementById('food-input').value;
+   foodInput.value = '';
+
+   const rentsInput = document.getElementById('rents-input').value;
+   rentsInput.value = '';
+
+   const clothInput = document.getElementById('cloth-input').value;
+   clothInput.value = '';
+
+   const totalexpense = parseFloat(foodInput) + parseFloat(rentsInput) + parseFloat(clothInput);
+
+   const FinalTotalExpense = document.getElementById('total-expense');
+   FinalTotalExpense.innerText = parseFloat(totalexpense.toFixed(2));
+
+   const incomeInput = document.getElementById('income-input').value;
+   const balance = document.getElementById('balance');
+   const balanceAmount = balance.innerText; 
+   balance.innerText = parseFloat(incomeInput) - parseFloat(totalexpense);
+})
+
+document.getElementById('interest-button').addEventListener('click',function(){
+   const incomeInput = document.getElementById('income-input').value;
+   const interest = document.getElementById('interest').value;
+   const totalInterest = parseFloat(incomeInput) / parseFloat(interest); 
+   const savings = document.getElementById('savings');
+   savings.innerText = totalInterest;
+
+   const remainingBalance = document.getElementById('remaining-balance');
+   remainingBalance.innerText = balance.innerText - totalInterest;
+})
